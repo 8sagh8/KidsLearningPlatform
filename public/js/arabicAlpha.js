7d79def873cbac17c6efc41f65615a7fbd63b17b
SAGH <42285767+8sagh8@users.noreply.github.com>
2021-01-04 14:34:58 -0500

    working on sound of arabic alpha

--- public/js/arabicAlpha.js
+++ public/js/arabicAlpha.js
@@ -1,11 +1,66 @@
+// Importing Anchor Button
 let button_Alif = document.querySelector("#aAlif");
+let button_Ba = document.querySelector("#aBa");
+let button_Ta = document.querySelector("#aTa");
+let button_Tha = document.querySelector("#aTha");
+let button_Jiim = document.querySelector("#aJiim");
+let button_Hha = document.querySelector("#aHha");
+let button_Kha = document.querySelector("#aKha");
+let button_Daal = document.querySelector("#aDaal");
+let button_Thaal = document.querySelector("#aThaal");
+let button_Ra = document.querySelector("#aRa");
+let button_Zay = document.querySelector("#aZay");
+let button_Siin = document.querySelector("#aSiin");
+let button_Shiin = document.querySelector("#aShiin");
+let button_Saad = document.querySelector("#aSaad");
+let button_Daad = document.querySelector("#aDaad");
+let button_Taa = document.querySelector("#aTaa");
+let button_Thaa = document.querySelector("#aThaa");
+let button_Ayn = document.querySelector("#aAyn");
+let button_Ghayn = document.querySelector("#aGhayn");
+let button_Fa = document.querySelector("#aFa");
+let button_Qaf = document.querySelector("#aQaf");
+let button_Kaf = document.querySelector("#aKaf");
+let button_Lam = document.querySelector("#aLam");
+let button_Miim = document.querySelector("#aMiim");
+let button_Nuun = document.querySelector("#aNuun");
+let button_Ha = document.querySelector("#aHa");
+let button_Waw = document.querySelector("#aWaw");
+let button_Ya = document.querySelector("#aYa");
 
-let audio_Alif = new Audio('/audios/arabicAlpha/alif.mp3');
+
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
+let audio_Alif = new Audio("audios/arabicAlpha/alif.mp3");
 
 
 
 let playAlif = () => {
-    prompt("alix");
     audio_Alif.play();
 }
 
